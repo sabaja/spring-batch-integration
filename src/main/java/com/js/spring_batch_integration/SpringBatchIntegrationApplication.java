b908d4386be5c6cffe7fe791bbe0803a71d34a60
sabaja <jacoposabatini76@gmail.com>
2024-07-07 18:56:45 +0200

    Cancella il file dopo copiato

--- src/main/java/com/js/spring_batch_integration/SpringBatchIntegrationApplication.java
+++ src/main/java/com/js/spring_batch_integration/SpringBatchIntegrationApplication.java
@@ -1,13 +1,31 @@
 package com.js.spring_batch_integration;
 
-import org.springframework.boot.SpringApplication;
+import com.js.spring_batch_integration.configuration.BasicIntegrationConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class SpringBatchIntegrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchIntegrationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        AbstractApplicationContext context
+                = new AnnotationConfigApplicationContext(BasicIntegrationConfig.class);
+        context.registerShutdownHook();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter q and press <enter> to exit the program: ");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if ("q".equals(input.trim())) {
+                break;
+            }
+        }
+        System.exit(0);
+
+    }
 
 }
---
+++ src/main/java/com/js/spring_batch_integration/configuration/BasicIntegrationConfig.java
@@ -0,0 +1,59 @@
+package com.js.spring_batch_integration.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.filters.SimplePatternFileListFilter;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+import java.io.File;
+
+@Slf4j
+@Configuration
+@EnableIntegration
+public class BasicIntegrationConfig {
+    public String INPUT_DIR = "/home/sabaja/Desktop/Flowgorithm";
+    public String OUTPUT_DIR = "/home/sabaja/Desktop/Casa/Roma";
+    public String FILE_PATTERN = "*.ods";
+
+    @Bean
+    public MessageChannel fileChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    @InboundChannelAdapter(value = "fileChannel", poller = @Poller(fixedDelay = "5000"))
+    public MessageSource<File> fileReadingMessageSource() {
+        FileReadingMessageSource sourceReader = new FileReadingMessageSource();
+        sourceReader.setDirectory(new File(INPUT_DIR));
+        sourceReader.setFilter(new SimplePatternFileListFilter(FILE_PATTERN));
+        log.info("Start reading");
+        return sourceReader;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "fileChannel")
+    public MessageHandler fileWritingMessageHandler() {
+        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(OUTPUT_DIR));
+        handler.setFileExistsMode(FileExistsMode.REPLACE);
+        handler.setExpectReply(false);
+        log.info("Start writing");
+        // Per cancellare il file dopo scritto
+        return h -> {
+//            log.info("Processing file: [{}]");
+            boolean isDelete = ((File) h.getPayload()).delete();
+            log.info("delete:[{}]", isDelete);
+        };
+
+    }
+}
